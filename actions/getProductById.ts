8fa29af2db49643974a37c321a22b72f311f0a50
Eduarda Cardoso <eduardac.brandao@hotmail.com>
2025-07-01 19:27:35 -0400

    commit

--- actions/getProductById.ts
+++ actions/getProductById.ts
@@ -11,59 +11,14 @@ export default async function getProductById(params: IParams) {
         if (!productId) return null
 
         const product = await prisma.product.findUnique({
-        where: {
-            id: productId
-        },
-    // include: {
-    //   reviews: {
-    //     include: {
-    //       user: true
-    //     },
-    //     orderBy: {
-    //       createdAt: 'desc' // Certifique-se de usar o nome correto do campo
-    //     }
-    //   }
-    // }
-    })
+            where: {
+                id: productId
+            },
+        })
 
         return product
-    } catch (error: any) {
-        throw new Error(error)
+    } catch (error) {
+        console.error("Erro em getProductById:", error)
+        return null 
     }
 }
-
-
-// import prisma from '@/libs/prismadb'
-
-// interface IParams{
-//     productId?: string
-// }
-
-// export default async function getProductById(params: IParams){
-//     try {
-//         const {productId} = params
-
-//         const product = await prisma.product.findUnique({
-//             where:{
-//                 id: productId
-//             },
-//             // include:{
-//             //     reviews:{
-//             //         include:{
-//             //             user: true
-//             //         },
-//             //         orderBy:{
-//             //             createdDate: 'desc'
-//             //         }
-//             //     }
-//             // }
-//         })
-
-//         if(!product){
-//             return null
-//         }
-//         return product
-//     } catch (error: any) {
-//         throw new Error(error)
-//     }
-// }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,7 @@
 export default function NotFound() {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-white text-slate-950">
-      <h1 className="text-4xl font-bold mb-4">Página não encontrada</h1>
-      <p className="text-lg text-gray-600">Desculpe, a página que você está procurando não existe.</p>
-    </div>
-  );
+    return (
+        <div className="min-h-screen flex flex-col justify-center items-center bg-white text-slate-950">
+            <h1 className="text-4xl font-bold mb-4">Página não encontrada</h1>
+        </div>
+    );
 }
