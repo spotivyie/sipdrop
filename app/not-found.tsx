4e1c47876d98e978e2b7a4f8d375396bf24379c1
Eduarda Cardoso <eduardac.brandao@hotmail.com>
2025-07-01 19:11:23 -0400

    commit

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <h1 className="text-3xl font-bold">404 - Página não encontrada</h1>
+        </div>
+    )
+}
