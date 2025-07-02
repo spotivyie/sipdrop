284d72f730555dc8f7c8b450e30ae41a1d146df8
Eduarda Cardoso <eduardac.brandao@hotmail.com>
2025-07-01 20:51:44 -0400

    commit

--- app/api/product/[id]/route.ts
+++ app/api/product/[id]/route.ts
@@ -2,22 +2,26 @@ export const runtime = 'nodejs';
 
 import getCurrentUser from "@/actions/getCurrentUser";
 import { NextResponse } from "next/server";
+import prisma from "@/libs/prismadb"; 
 
 export async function DELETE(
-    request: Request,
-    {params}: {params: {id: string}}
-){
-    const currentUser = await getCurrentUser()
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const currentUser = await getCurrentUser();
 
-    if(!currentUser) return NextResponse.error()
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
 
-    if(currentUser.role !== 'ADMIN'){
-        return NextResponse.error()
-    }
+  try {
+    const product = await prisma.product.delete({
+      where: { id: params.id },
+    });
 
-    const product = await prisma?.product.delete({
-        where: {id: params.id}
-    })
-
-    return NextResponse.json(product)
+    return NextResponse.json(product);
+  } catch (error) {
+    console.error("Erro ao deletar produto:", error);
+    return new NextResponse("Erro interno do servidor", { status: 500 });
+  }
 }
